a45e7b8243c0d7ecb7596c53309bacb5b8875bf7
reybahl <bahl.reyansh@gmail.com>
2025-05-01 11:34:33 -0400

    subsetted

---
+++ subsetted/src/CNN.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+class CNN(nn.Module):
+    
+    def __init__(self, num_classes: int):
+        super(CNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 28 * 28, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
